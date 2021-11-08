acd82108b7cc33008fb71e913370e322a3673fdb
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-11-08 11:56:04 +0100

    add author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   before do
-    author = Author.new(first_name='Alan', last_name='Turing',homepage='http://wikipedia.org/Alan_Turing')
+    @author = Author.new
+    @author.first_name='Alan' 
+    @author.last_name='Turing'
+    @author.homepage='http://wikipedia.org/Alan_Turing'
   end
 
   it 'should have user-defined fields ' do
-    except(author.first_name).to eq('Alan')
-    except(author.last_name).to eq('Turing')
+    expect(@author.first_name).to eq('Alan')
+    expect(@author.last_name).to eq('Turing')
   end
   it 'should have computed fields' do
-    expect(author.name).to eq('Alan Turing')
+    expect(@author.name).to eq('Alan Turing')
   end
 end
