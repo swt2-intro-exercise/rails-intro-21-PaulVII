b1213de0d6867e0c27e6f112f7914335200e621f
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-11-12 15:44:48 +0100

    lets try without the test

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
     last_name { "Turing" }
     homepage { "http://de.wikipedia.org/Alan_Turing" }
   end
+  factory :author2 do
+    first_name { "Alan2" }
+    last_name { "Turing2" }
+    homepage { "http://de.wikipedia.org/Alan_Turing2" }
+  end
 end
