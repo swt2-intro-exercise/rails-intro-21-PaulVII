77884ff9a5d0ed72df32999fb6069265220f7c33
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-11-19 15:20:07 +0100

    add ability to filter index by year

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
+    scope :from_year , -> (year) { where("year = ?", year) }
     has_and_belongs_to_many :authors
     validates :title, presence: true
     validates :year, presence: true, numericality: { only_integer: true }
